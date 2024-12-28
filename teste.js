367f894fe60b0a73ad75a152f0fc0425d7c1baa4
Henrique Siqueira <71899443+henriquersiqueiraa@users.noreply.github.com>
2024-12-28 12:04:48 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,17 @@
+const nome = "Henrique"
+console.log(nome)
+
+const soma = 2 + 2
+console.log(soma)
+
+const idade = 26
+console.log(idade)
+
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade * 2);
+console.log(idade / 2);
+console.log(idade % 2);
+console.log(idade ** 2);
+
+const idadeSomada = idade + 2;
